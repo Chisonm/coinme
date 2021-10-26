8e46f5fd7cbb2578a39a12687570d9380a089413
chisonm <danielchisonum@gmail.com>
2021-10-26 20:37:31 +0100

    hero section done

--- components/hero/Hero.js
+++ components/hero/Hero.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
-import logo from '../../public/logo.png'
+import hero from '../../public/Group 10.png'
+import HeroStyles from '../../styles/Hero.module.css'
 
 const Hero = () => {
     return (
-        <div style={{ color: 'white'}}>
-            <h1 style={{ color: 'white'}}>hello</h1>
-
+        <div className={HeroStyles.container}>
+           <div className={HeroStyles.d_flex}>
+           <div className={HeroStyles.hero_content}>
+                <h2 className={HeroStyles.hero_header}>Trade crypto seamlessly and anywhere</h2>
+                <p className={HeroStyles.hero_lead}>Coinmeis the easiest place to buy and sell cryptocurrencies.
+                    Sign up and get started today.
+                </p>
+            </div>
+            <div className={HeroStyles.image_right}>
+                <Image  src={hero} />
+            </div>
+           </div>
         </div>
     )
 }
